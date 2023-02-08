23ea397555efd27699cd6afa99970f9b321a202e
leonardos4enz <leonardorsaenz@gmail.com>
2023-02-08 11:51:29 -0600

    Widgets Separados

--- lib/main.dart
+++ lib/main.dart
@@ -33,23 +33,72 @@ class _InicioState extends State<Inicio> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text("Filas, columnas y botones"),
-      ),
-      body: Center(
-          child: ElevatedButton(
-        child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
-          Icon(Icons.access_time),
-          SizedBox(
-            height: 10,
-          ),
-          Text("Ahora")
-        ]),
-        onPressed: () {
-          var t = DateTime.now();
-          print(t);
-        },
-      )),
-    );
+        //appBar: AppBar(
+        //  title: const Text("Filas, columnas y botones"),
+        //  ),
+        body: cuerpo());
   }
+}
+
+Widget cuerpo() {
+  return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: NetworkImage("https://fondosmil.com/fondo/23222.jpg"),
+              fit: BoxFit.cover)),
+      child: Center(
+          child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: <Widget>[
+          nombre(),
+          campoUsuario(),
+          campoContrasena(),
+          SizedBox(
+            height: 15,
+          ),
+          botonEntrar(),
+        ],
+      )));
+}
+
+Widget nombre() {
+  return Text("Sign in",
+      style: TextStyle(
+          color: Color.fromARGB(255, 255, 255, 255),
+          fontSize: 35.0,
+          fontWeight: FontWeight.bold));
+}
+
+Widget campoUsuario() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 20, vertical: 20),
+    child: TextField(
+        decoration: InputDecoration(
+      hintText: "User",
+      fillColor: Colors.white,
+      filled: true,
+    )),
+  );
+}
+
+Widget campoContrasena() {
+  return Container(
+    padding: EdgeInsets.symmetric(horizontal: 20),
+    child: TextField(
+        obscureText: true,
+        decoration: InputDecoration(
+          hintText: "Password",
+          fillColor: Colors.white,
+          filled: true,
+        )),
+  );
+}
+
+Widget botonEntrar() {
+  return ElevatedButton(
+      onPressed: () {},
+      child: Text(
+        "Enter",
+        style: TextStyle(fontSize: 25, color: Colors.white),
+      ));
 }
